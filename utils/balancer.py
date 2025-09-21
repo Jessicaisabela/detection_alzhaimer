39e06667e729e150bc684650a6d428f3ac182286
Jessicaisabela <Jessicaisacastro@gmail.com>
2025-09-21 00:13:54 -0300

    chore: criação de pre processamento dos dados

---
+++ utils/balancer.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+from imblearn.over_sampling import SMOTE 
+
+input_train = pd.read_csv(r'C:\Users\jessi\OneDrive\Área de Trabalho\detection_alzhaimer\archive\train\input_train.csv')
+
+output_train = pd.read_csv(r'C:\Users\jessi\OneDrive\Área de Trabalho\detection_alzhaimer\archive\train\output_train.csv')
+
+original_columns = input_train.columns
+input_train_numeric = pd.get_dummies(input_train, drop_first=True)
+
+sm = SMOTE(random_state=42)
+input_train_balanced, output_train_balanced = sm.fit_resample(input_train_numeric, output_train)
+
+print(output_train_balanced.value_counts())
+
+input_train_balanced.to_csv(r'C:\Users\jessi\OneDrive\Área de Trabalho\detection_alzhaimer\archive\train\input_train_balanced.csv', index=False)
+output_train_balanced.to_csv(r'C:\Users\jessi\OneDrive\Área de Trabalho\detection_alzhaimer\archive\train\output_train_balanced.csv', index=False)
